87e9c6c44fdfdc0266a196a846a471a0252d4498
Aimee <93619242@qq.com>
2022-01-03 22:19:24 +0800

    third commit

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git-maven");
         System.out.println("hello nihao");
+        System.out.println("hello nixx");
     }
 }
