1e504045d553bf4860bca59f996bfa9e6d9fdfed
Aimee <93619242@qq.com>
2022-01-03 22:18:39 +0800

    second commit

---
+++ src/main/java/org/example/Hello.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Author wangyufei
+ * @Date 2022/1/3 22:11
+ * @Info
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git-maven");
+        System.out.println("hello nihao");
+    }
+}
